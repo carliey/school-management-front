f5d33805aacdee6c95929e7f70d1825ac8bad2ef
khaleefa <mdcarliey@gmail.com>
2023-02-03 22:49:35 +0100

    added a create teache form

--- src/pages/teachers/CreateTeacherModal.tsx
+++ src/pages/teachers/CreateTeacherModal.tsx
@@ -5,6 +5,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import { Grid, TextField, Box, Select, MenuItem, Stack } from "@mui/material";
 
 interface ComponentProps {
   open: boolean;
@@ -15,6 +16,11 @@ export default function CreateTeacherModal({
   open,
   handleClose,
 }: ComponentProps) {
+  const classrooms = [
+    { id: 1, name: "primary one" },
+    { id: 2, name: "primary two" },
+    { id: 3, name: "primary three" },
+  ];
   return (
     <div>
       <Dialog
@@ -23,15 +29,71 @@ export default function CreateTeacherModal({
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
+        <DialogTitle id="alert-dialog-title">Create Teacher</DialogTitle>
+        <Box p={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <TextField
+                id="outlined-basic"
+                label="First name *"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <TextField
+                id="outlined-basic"
+                label="Middle name"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <TextField
+                id="outlined-basic"
+                label="Last name *"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <TextField
+                id="outlined-basic"
+                label="Phone *"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <TextField
+                id="outlined-basic"
+                label="Email "
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Select displayEmpty fullWidth defaultValue={""}>
+                <MenuItem value="">Select gender</MenuItem>
+                <MenuItem value="male">Male</MenuItem>
+                <MenuItem value="female">Female</MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12}>
+              <Select displayEmpty fullWidth defaultValue={""}>
+                <MenuItem value="">Select Classroom</MenuItem>
+                {classrooms.map((item) => (
+                  <MenuItem key={item.id} value={item.name}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+          </Grid>
+          <Stack gap={2} direction="row" justifyContent={"flex-end"} mt={2}>
+            <Button variant="contained" color="error">
+              Cancel
+            </Button>
+            <Button variant="contained" color="primary">
+              Save
+            </Button>
+          </Stack>
+        </Box>
       </Dialog>
     </div>
   );
