234780a09a08838bfb2b232ffa89f053ed39c51a
carliey <mdcarliey@gmail.com>
2023-05-12 12:09:43 +0100

    added theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux/es/exports";
@@ -6,13 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import store from "./redux/store";
+import { theme } from "./theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CssBaseline />
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e7d32",
+    },
+    text: {
+      primary: "#333333",
+      secondary: "#B1B1B1",
+    },
+  },
+});
